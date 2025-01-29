626af524052a508840e64b33632f76ab652efdd3
Philippe Menard <phil.menard1668@gmail.com>
2025-01-29 14:40:59 +0100

    finished with first bonus

--- pipex.c
+++ pipex.c
@@ -6,16 +6,17 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:23:50 by pmenard           #+#    #+#             */
-/*   Updated: 2025/01/28 19:48:58 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/01/29 14:19:00 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	check_path_errors(char **argv, char **env, int i)
+void	check_path_errors(char **argv, char **env, int i, int *pipefd)
 {
 	char	*path;
 
+	close(*pipefd);
 	path = get_right_path(env, argv[i]);
 	if (!path)
 	{
@@ -34,10 +35,10 @@ void	exec_cmds(int previous_fd, int next_fd, char *argv, char **env)
 	char	**arg;
 
 	if (dup2(previous_fd, STDIN_FILENO) == -1)
-		return (perror("dup2in"), exit(EXIT_FAILURE));
+		exit(EXIT_FAILURE);
 	close(previous_fd);
 	if (dup2(next_fd, STDOUT_FILENO) == -1)
-		return (perror("dup2out"), exit(EXIT_FAILURE));
+		exit(EXIT_FAILURE);
 	close(next_fd);
 	path = get_right_path(env, argv);
 	arg = fill_arg(path, argv);
@@ -61,14 +62,14 @@ void	pipex(char **argv, char **env, int previous_fd, int outfile)
 	{
 		if (pipe(pipefd) == -1)
 			return (perror("pipe"), exit(EXIT_FAILURE));
-		id = fork();
-		if (id == -1)
+		if (previous_fd != -1)
+			id = fork();
+		if (previous_fd != -1 && id == -1)
 			return (perror("fork"), exit(EXIT_FAILURE));
-		if (id == 0)
+		if (previous_fd != -1 && id == 0)
 		{
-			close(pipefd[0]);
-			check_path_errors(argv, env, i);
-			if (i == (argv_size(argv) - 1))
+			check_path_errors(argv, env, i, &pipefd[0]);
+			if (i == (argv_size(argv) - 1) && outfile != -1)
 				pipefd[1] = outfile;
 			exec_cmds(previous_fd, pipefd[1], argv[i], env);
 		}
@@ -89,21 +90,21 @@ int	main(int argc, char **argv, char **env)
 	{
 		previous_fd = open(argv[1], O_RDONLY);
 		if (previous_fd == -1)
-		{
-			perror("open");
-			exit(EXIT_FAILURE);
-		}
+			perror(argv[1]);
 		outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (outfile == -1)
-		{
-			perror("open");
-			exit(EXIT_FAILURE);
-		}
+			perror(argv[argc - 1]);
 		args = get_cmds(argv, argc);
 		if (args == NULL)
 			return (close(previous_fd), close(outfile), 1);
 		pipex(args, env, previous_fd, outfile);
 		free_db_array(args);
+		return (0);
 	}
-	return (0);
+	else
+	{
+		ft_printf("Not enough arguments. ");
+		ft_printf("Should be ./pipex file1 cmd1 cmd2 ... cmdn file2\n");
+		return (1);
+	}
 }
